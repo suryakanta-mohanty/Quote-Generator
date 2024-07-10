94b2cc993b7cc7f845209c29dc8135683637ee78
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-07-10 14:47:01 +0530

    adding api to the project

---
+++ index.js
@@ -0,0 +1,20 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+const api_url = 'https://api.quotable.io/random';
+
+
+async function getQuote(url){
+  const  response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+  quote.innerHTML = `${data.content}`;
+  author.innerHTML = data.author;
+  
+}
+
+getQuote(api_url);
+
+document.getElementById('js-btn').addEventListener('click', () =>{
+  getQuote(api_url);
+})
