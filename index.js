54277d7a57050c05fcd9fab2815b4b3f3565e607
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-07-10 15:09:03 +0530

    adding twitter share line button and making it responsive

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const api_url = 'https://api.quotable.io/random';
 async function getQuote(url){
   const  response = await fetch(url);
   var data = await response.json();
-  console.log(data);
+  // console.log(data);
   quote.innerHTML = `${data.content}`;
   author.innerHTML = data.author;
   
@@ -17,4 +17,12 @@ getQuote(api_url);
 
 document.getElementById('js-btn').addEventListener('click', () =>{
   getQuote(api_url);
+});
+
+document.getElementById('js-tweet-btn').addEventListener('click', ()=>{
+  tweet();
 })
+
+function tweet(){
+  window.open(`https://twitter.com/intent/tweet?text=${quote.innerHTML} ---- by ${author.innerHTML}`, 'Tweet Window', 'width= 600', 'height = 300');
+}
